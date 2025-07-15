0ca6f976a763bb8b2c45d9cae8653d6d6f17e6e4
IvanPavlovets <ivan150287@gmail.com>
2025-07-15 22:47:44 +0700

    Init commit

---
+++ src/main/java/com/example/AuthApiApp/AuthApiAppApp.java
@@ -0,0 +1,13 @@
+package com.example.AuthApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiAppApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApiAppApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthApiApp/AuthApiAppAppTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiAppAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
